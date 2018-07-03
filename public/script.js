eea718c1f918a25a7422fad760d3d1b562567b54
Ben Bracamonte <ben.bracamonte@gmail.com>
2018-07-03 14:22:46 -0500

    budiling cards correctly

--- public/script.js
+++ public/script.js
@@ -13,10 +13,20 @@ $(document).ready(function(){
 }) // end ready
 
 populateList = (data) => {
-  let listHTML
+  let listHTML = ''
   const ul = $('.list-group')
 
-  // data.books.forEach(book => {
-
-  // })
+  data.books.forEach(book => {
+    listHTML += `<div class="card mb-5" style="width: 18rem;">`
+    listHTML += `<img class="card-img-top" src="${book.image}" alt="Image of ${book.title}"`
+    listHTML += `<div class="card-body">`
+    listHTML += `<h5 class="card-title mt-2">${book.title}</h5>`
+    listHTML += `<h6 class="card-subtitle mb-2 text-muted">${book.author}</h6>`
+    listHTML += `<p class="card-text">${book.releaseDate}</p>`
+    listHTML += `<div class="card-buttons">`
+    listHTML += `<button type="button" class="mr-2 mb-3 editBtn btn btn-info">Edit</button>`
+    listHTML += `<button type="button" class="ml-2 mb-3 deleteBtn btn btn-danger">Delete</button>`
+    listHTML += `</div></div></div>`
+  })
+  ul.html(listHTML)
 }
